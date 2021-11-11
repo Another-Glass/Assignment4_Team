b8b824ca843f9845a01d9ca2daf01e8393e96762
const <epitoneplus@gmail.com>
2021-11-11 23:45:26 +0900

    [feat]: 계좌생성 API 추가 (#7)
    1. account 모델의 salt 컬럼 추가
    2. service, controller의 로직 추가
    3. 미들웨어 토큰 값 수정
    4. 입력값 변수 수정

--- src/controllers/accountController.js
+++ src/controllers/accountController.js
@@ -2,10 +2,30 @@ const { statusCode, responseMessage } = require('../globals');
 const { resFormatter } = require('../utils');
 const accountService = require('../services/accountService.js');
 const logger = require('../utils/logger');
-
+const ValidationError = require('../utils/errors/commonError');
+const encryption = require('../libs/encryption');
 
   //계좌생성
-exports.getAccount = async(res,req,next) => {
+exports.postAccount = async(req, res, next) => {
+  const { accountPassword } = req.body;
+  const { id } = req.decoded;
 
+  if(accountPassword === undefined || id === undefined) {
+    throw new ValidationError()
+  }
+  
+  const salt = encryption.makeSalt();
+  const encryptPassword = encryption.encrypt(accountPassword, salt);
+  
+  const dto = {
+    id, 
+    password: encryptPassword,
+    salt
+  }
+
+  const newAccount = await accountService.createAccount(dto)
+  return res
+        .status(statusCode.CREATED)
+        .send(resFormatter.success(responseMessage.ACCOUNT_CREATED, newAccount))
 }
   
--- src/routes/accountRouter.js
+++ src/routes/accountRouter.js
@@ -3,11 +3,12 @@ const routes = require('../globals/routes');
 
 const accountController = require('../controllers/accountController.js');
 const transactionController = require('../controllers/transactionController.js');
+const auth = require('../middlewares/auth');
 
 const accountRouter = express.Router();
 
-//유저생성
-accountRouter.post(routes.root, accountController.getAccount);
+//계좌 생성
+accountRouter.post(routes.root, auth.checkToken, accountController.postAccount);
 
 //로그인
 accountRouter.post(routes.accountDetail+routes.transaction, transactionController.postTransaction);
