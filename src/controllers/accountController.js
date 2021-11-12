39706a4ba1182c2a7ee3977c6bf97501d5ea3c20
SibaDoge1 <rladus753159@gmail.com>
2021-11-12 17:35:50 +0900

    [fix]: 에러핸들링 추가
    - auth 파일의 토큰관련 에러핸들링 추가, 구조개선
    - accountController 에러핸들링 추가
    - database.db 경로 잘못되었던거 수정

--- src/controllers/accountController.js
+++ src/controllers/accountController.js
@@ -7,27 +7,31 @@ const encryption = require('../libs/encryption.js');
 const logger = require('../utils/logger');
 const ValidationError = require('../utils/errors/commonError');
 
-  //계좌생성
-exports.postAccount = async(req, res, next) => {
-  const { accountPassword } = req.body;
-  const { userId } = req.decoded;
+//계좌생성
+exports.postAccount = async (req, res, next) => {
+  try {
+    const { accountPassword } = req.body;
+    const { userId } = req.decoded;
 
-  if(accountPassword === undefined || userId === undefined) {
-    throw new ValidationError()
-  }
-  
-  const salt = encryption.makeSalt();
-  const encryptPassword = encryption.encrypt(accountPassword, salt);
-  
-  const dto = {
-    userId, 
-    password: encryptPassword,
-    salt
-  }
+    if (accountPassword === undefined || userId === undefined) {
+      throw new ValidationError()
+    }
 
-  const newAccount = await accountService.createAccount(dto)
-  return res
-        .status(statusCode.CREATED)
-        .send(resFormatter.success(responseMessage.ACCOUNT_CREATED, {accountNumber:newAccount.accountNumber}))
+    const salt = encryption.makeSalt();
+    const encryptPassword = encryption.encrypt(accountPassword, salt);
+
+    const dto = {
+      userId,
+      password: encryptPassword,
+      salt
+    }
+
+    const newAccount = await accountService.createAccount(dto)
+    return res
+      .status(statusCode.CREATED)
+      .send(resFormatter.success(responseMessage.ACCOUNT_CREATED, { accountNumber: newAccount.accountNumber }))
+  } catch (err) {
+    next(err);
+  }
 }
   
